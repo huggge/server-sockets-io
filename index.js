be7381ca37be6c8838712862e0723d50167af75a
Hugo Vestberg <55721402+huggge@users.noreply.github.com>
2021-05-23 19:38:27 +0200

    fix login emit

--- index.js
+++ index.js
@@ -1,25 +1,32 @@
-var express = require('express');
-const { emit } = require('nodemon');
-var socket = require('socket.io');
-
+var express = require('express')
+var socket = require('socket.io')
 
 // App setup
-var app = express();
-var server = app.listen(1337, () => console.log("listening on port 1337"));
-
+var app = express()
+var server = app.listen(1337, () => console.log('listening on port 1337'))
 
 app.use(express.static('public'))
 
 // Socket setup
-var io = socket(server);
+var io = socket(server)
 
 io.on('connection', (socket) => {
-    console.log("made socket connection")
-    console.log("idet", socket.id)
-    
-    socket.on("message", (data) => {
-        console.log("datta", data)
-        socket.broadcast.emit('message', data)
-    })
+  console.log('made socket connection')
+  console.log('idet', socket.id)
+
+  socket.on('message', (data) => {
+    data.id = socket.id
+    console.log()
+    // socket.broadcast.emit('message', data,)
+    io.emit('message', usersOnline)
+  })
+
+  socket.on('login', (data) => {
+    console.log('login')
+    data.id = socket.id
+    console.log(data)
+       io.emit('isOnline', data)
+    // socket.broadcast.emit('isOnline', data)
+  })
 
 })
